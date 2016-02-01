f242725847c0f7f09a3280e71802b0f4a5cfa7e4
sohamparekh92 <soham.parekh92@gmail.com>
2016-01-31 22:47:24 -0800

    Initial commit

---
+++ app/src/main/java/com/example/soham/doodle/DrawingView.java
@@ -0,0 +1,23 @@
+package com.example.soham.doodle;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+/**
+ * Created by Soham on 1/31/2016.
+ */
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context,attrs);
+
+        setMyDraw();
+    }
+
+    private void setMyDraw(){
+
+    }
+}
