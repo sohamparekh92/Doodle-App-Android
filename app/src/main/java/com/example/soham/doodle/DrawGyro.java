ab2d6844bb764dc0c36570bd2b9671f515c8b04b
sohamparekh92 <soham.parekh92@gmail.com>
2016-02-03 18:30:29 -0800

    Added new Activity

---
+++ app/src/main/java/com/example/soham/doodle/DrawGyro.java
@@ -0,0 +1,13 @@
+package com.example.soham.doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawGyro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_gyro);
+    }
+}
